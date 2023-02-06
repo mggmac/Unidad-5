99affa174252dee0af499135c3d29dc1d36cf1c2
mggmac <124072365+mggmac@users.noreply.github.com>
2023-02-06 15:23:15 +0100

    Create ejercicio6.js

---
+++ HTML/Javascript/ejercicio6.js
@@ -0,0 +1,8 @@
+const palabra1='enviromental';
+console.log(palabra1[0].toUpperCase() + palabra1.substring(1));
+const palabra2='systems';
+console.log(palabra2[0].toUpperCase() + palabra2.substring(1));
+const palabra3='research';
+console.log(palabra3[0].toUpperCase() + palabra3.substring(1));
+const palabra4='institute';
+console.log(palabra4[0].toUpperCase() + palabra4.substring(1));
