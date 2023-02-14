ae841920e9d3a5dfc6a56544d3cc8f574081ef14
mggmac <124072365+mggmac@users.noreply.github.com>
2023-02-14 19:35:33 +0100

    correcto

---
+++ HTML/PruebaFinalCV/curriculum.js
@@ -0,0 +1,23 @@
+
+
+function mostrarExperiencia(){
+    if(document.getElementById('experienciaLaboral').style.display==='none')/*aquí se debe poner === para que compare, un = establece el valor del display*/
+    return document.getElementById('experienciaLaboral').style.display='block'
+    else
+    return document.getElementById('experienciaLaboral').style.display='none'
+    
+}
+
+function mostrarFormacion(){
+    if(document.getElementById('formacion').style.display==='none')
+    return document.getElementById('formacion').style.display='block'
+    else
+    return document.getElementById('formacion').style.display='none'
+}
+
+function mostrarDatos(){
+    if(document.getElementById('otrosDatos').style.display==='none')
+    return document.getElementById('otrosDatos').style.display='block'
+    else
+    return document.getElementById('otrosDatos').style.display='none'
+}
